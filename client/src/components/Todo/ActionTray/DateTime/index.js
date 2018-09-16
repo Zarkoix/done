4129c4ca31ec9fc2f8a3cbd1107b8ef1b48172cd
Zarkoix <adam.towers77@gmail.com>
2018-09-16 16:48:33 -0700

    skeleton calendar icon in action tray

--- client/src/components/Todo/ActionTray/DateTime/index.js
+++ client/src/components/Todo/ActionTray/DateTime/index.js
@@ -1,19 +1,22 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
 
-import DateButton from './DateButton.js'
-import TimeButton from './TimeButton.js'
+import CalendarIcon from "@material-ui/icons/CalendarToday";
+import DateButton from "./DateButton.js";
+import TimeButton from "./TimeButton.js";
 
 class DateTime extends Component {
   render() {
     const { classes } = this.props;
     return (
       <React.Fragment>
+        <IconButton className={classes.button} aria-label="Change planned time">
+          <CalendarIcon />
+        </IconButton>
         <DateButton />
         <TimeButton />
-
       </React.Fragment>
     );
   }
