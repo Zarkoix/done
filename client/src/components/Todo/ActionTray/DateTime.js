447bd33ec9a543eb3e91f1fe85ca2711490d99ec
Zarkoix <adam.towers77@gmail.com>
2018-09-16 14:06:49 -0700

    skeleton interface for date and time

--- client/src/components/Todo/ActionTray/DateTime.js
+++ client/src/components/Todo/ActionTray/DateTime.js
@@ -1,3 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
 
-export default () => <span>date and time</span>;
+const styles = theme => ({
+  buttonOutline: {
+    alignSelf: "center",
+    padding: theme.spacing.unit + "px",
+    margin: theme.spacing.unit + "px",
+    borderRadius: "5px",
+    background: theme.palette.divider,
+    color: theme.palette.background.paper
+  }
+});
+
+class DateTime extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <React.Fragment>
+        <Typography className={classes.buttonOutline} variant="button">
+          Sep. 24
+        </Typography>
+        <Typography className={classes.buttonOutline} variant="button">
+          7:00
+        </Typography>
+      </React.Fragment>
+    );
+  }
+}
+
+DateTime.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(DateTime);
