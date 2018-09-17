276d184bf4fca02a7cd8381d86327e4f694baad3
Zarkoix <adam.towers77@gmail.com>
2018-09-17 12:18:40 -0700

    update primary color

--- client/src/theme.js
+++ client/src/theme.js
@@ -4,17 +4,20 @@ export const DARK_MODE_LOC = "isDarkTheme";
 
 export const isDarkTheme = () => Boolean(localStorage.getItem(DARK_MODE_LOC));
 
-export const setDarkTheme = (bool=true) => {
+export const setDarkTheme = (bool = true) => {
   if (bool) {
-    localStorage.setItem(DARK_MODE_LOC, true)
+    localStorage.setItem(DARK_MODE_LOC, true);
   } else {
-    localStorage.removeItem(DARK_MODE_LOC)
+    localStorage.removeItem(DARK_MODE_LOC);
   }
-}
+};
 
 const theme = createMuiTheme({
   palette: {
-    type: isDarkTheme() ? "dark" : "light"
+    type: isDarkTheme() ? "dark" : "light",
+    primary: {
+      main: "#593f62"
+    }
   }
 });
 
