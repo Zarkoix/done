14be88852e63e59960c953d5d4b9bc73d79fbebe
Zarkoix <adam.towers77@gmail.com>
2018-09-18 23:06:52 -0700

    fix proptypes import on tags

--- client/src/components/Tag/index.js
+++ client/src/components/Tag/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import classNames from "classnames";
 import CloseIcon from "@material-ui/icons/Close";
