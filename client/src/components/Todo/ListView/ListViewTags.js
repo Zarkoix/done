460bd7a376c7d4098f92100b086406d216d7930e
Zarkoix <adam.towers77@gmail.com>
2018-09-20 13:47:11 -0700

    show tags in list view

--- client/src/components/Todo/ListView/ListViewTags.js
+++ client/src/components/Todo/ListView/ListViewTags.js
@@ -1,8 +1,25 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 
 import Tag from "../../Tag";
+
+const GET_TODO_TAGS = gql`
+  query getCompleteTodoData($id: Int!) {
+    todoById(id: $id) {
+      id
+      getTags {
+        nodes {
+          id
+          name
+          color
+        }
+      }
+    }
+  }
+`;
 
 const styles = theme => ({
   container: {
@@ -14,13 +31,22 @@ const styles = theme => ({
 
 class ListViewTags extends Component {
   render() {
-    const { classes } = this.props;
+    const { classes, id } = this.props;
     return (
-      <div className={classes.container}>
-        <Tag title="Red" color="#ffb3ba" />
-        <Tag title="Orange" color="#ffdfba" />
-        <Tag title="+3 more" color="#d8d8d8" />
-      </div>
+      <Query query={GET_TODO_TAGS} variables={{ id: id }}>
+        {({ loading, error, data }) => {
+          if (loading) return null;
+          if (error) return `Error!: ${error}`;
+          let tags = data.todoById.getTags.nodes;
+          return (
+            <div className={classes.container}>
+              {tags.map(tag => (
+                <Tag title={tag.name} key={tag.id} color={tag.color} />
+              ))}
+            </div>
+          );
+        }}
+      </Query>
     );
   }
 }
