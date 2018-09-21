7bfc3b93ce384bcc2052b5ef9780ec1af31585b1
Zarkoix <adam.towers77@gmail.com>
2018-09-20 18:02:41 -0700

    cleanup EditTagsItem imports

--- client/src/routes/App/Tags/EditTagsModal/EditTagsItem/index.js
+++ client/src/routes/App/Tags/EditTagsModal/EditTagsItem/index.js
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
-import IconButton from "@material-ui/core/IconButton";
-import DeleteIcon from "@material-ui/icons/Delete";
-import { Query } from "react-apollo";
 
 import DeleteButton from "./DeleteButton"
 
